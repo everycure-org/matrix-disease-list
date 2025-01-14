a69ef8f7d695871b7ceb9b651d1fe134d196ae3e
Marcello DeLuca <delucam14@gmail.com>
2025-01-14 13:52:13 -0500

    fully automate disease categorization upon building list

--- build-list.sh
+++ build-list.sh
@@ -1,1 +1,2 @@
 VERSION=v1.5.2 ./odk.sh make matrix-disease-list.tsv -B
+chmod +x categorize_diseases.sh && ./categorize_diseases.sh
--- categorize_diseases.sh
+++ categorize_diseases.sh
@@ -1,4 +1,5 @@
 chmod +x setup_ollama.sh && ./setup_ollama.sh
+chmod +x install_kedro.sh && ./install_kedro.sh
 cd llm-disease-categorization
 uv venv
 source .venv/bin/activate
