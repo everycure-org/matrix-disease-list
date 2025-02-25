67bf1d30638aff01c5e593f5f295a28581c458ac
Marcello DeLuca <delucam14@gmail.com>
2025-02-25 03:18:42 -0500

    make ec tags individual kedro nodes and regenerate list

--- llm-disease-categorization/src/llm_disease_categorization/pipelines/add_disease_categories/pipeline.py
+++ llm-disease-categorization/src/llm_disease_categorization/pipelines/add_disease_categories/pipeline.py
@@ -34,22 +34,87 @@ def create_pipeline(**kwargs) -> Pipeline:
             outputs = "disease_list_with_anatomical_tags",
             name = "add-tags-anatomical"
         ),
+        # node(
+        #     func=nodes.enrich_disease_list,
+        #     inputs=[
+        #         "disease_list_with_anatomical_tags",
+        #         "params:preprocessing.enrichment_tags",
+        #     ],
+        #     outputs="ec_enriched_disease_list",
+        #     name="enrich_disease_list",
+        # ),
+
         node(
-            func=nodes.enrich_disease_list,
-            inputs=[
+            func=nodes.enrich_disease_list_single_tag,
+            inputs = [
                 "disease_list_with_anatomical_tags",
-                "params:preprocessing.enrichment_tags",
+                "params:preprocessing.enrichment_tags.single_input.pathogen.input_params.output_col",
+                "params:preprocessing.enrichment_tags.single_input.pathogen",
+                "params:preprocessing.enrichment_tags.single_input.pathogen.single_input",
             ],
-            outputs="ec_enriched_disease_list",
-            name="enrich_disease_list",
+            outputs = "enriched_disease_list_pathogen",
+            name="add-pathogen-tag"
         ),
+
+        node(
+            func=nodes.enrich_disease_list_single_tag,
+            inputs = [
+                "enriched_disease_list_pathogen",
+                "params:preprocessing.enrichment_tags.single_input.cancer.input_params.output_col",
+                "params:preprocessing.enrichment_tags.single_input.cancer",
+                "params:preprocessing.enrichment_tags.single_input.cancer.single_input",
+            ],
+            outputs = "enriched_disease_list_cancer",
+            name="add-cancer-tag"
+        ),
+
+        node(
+            func=nodes.enrich_disease_list_single_tag,
+            inputs = [
+                "enriched_disease_list_cancer",
+                "params:preprocessing.enrichment_tags.single_input.glucose_dysfunction.input_params.output_col",
+                "params:preprocessing.enrichment_tags.single_input.glucose_dysfunction",
+                "params:preprocessing.enrichment_tags.single_input.glucose_dysfunction.single_input",
+            ],
+            outputs = "enriched_disease_list_glucose_dysfunction",
+            name="add-glucose_dysfunction-tag"
+        ),
+
+        node(
+            func=nodes.enrich_disease_list_single_tag,
+            inputs = [
+                "enriched_disease_list_glucose_dysfunction",
+                "params:preprocessing.enrichment_tags.single_input.existing_treatment.input_params.output_col",
+                "params:preprocessing.enrichment_tags.single_input.existing_treatment",
+                "params:preprocessing.enrichment_tags.single_input.existing_treatment.single_input",
+            ],
+            outputs = "enriched_disease_list_existing_treatment",
+            name="add-existing_treatment-tag"
+        ),
+
+        node(
+            func=nodes.enrich_disease_list_single_tag,
+            inputs = [
+                "enriched_disease_list_existing_treatment",
+                "params:preprocessing.enrichment_tags.multiple_input.quality_life_years_lost.input_params.output_col",
+                "params:preprocessing.enrichment_tags.multiple_input.quality_life_years_lost",
+                "params:preprocessing.enrichment_tags.multiple_input.quality_life_years_lost.single_input",
+            ],
+            outputs = "enriched_disease_list_qoly",
+            name="add-qoly-tag"
+        ),
+
+
+
+
         node(
             func=nodes.return_final_categories,
             inputs=[
-                "ec_enriched_disease_list",
+                "enriched_disease_list_qoly",
             ],
             outputs="disease_categories",
             name = "return-final-disease-categories"
         ),
+
 
     ])
